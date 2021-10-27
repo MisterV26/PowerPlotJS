d8e4a298a1728c7d2ad24171df486c59c9375b3e
vworks <dpenlan1@kent.edu>
2021-10-26 23:45:53 -0400

    initialize project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Polymap from './component/polymap.tsx'
+
+function App() {
+  let pmap = new Polymap();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/polymap.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export default class Polymap extends React.Component{
+    constructor(props:Readonly<object>){
+        super(props);
+
+        test:Number;
+    }
+    render(){
+        return(
+            {test}
+        )
+    }
+}
